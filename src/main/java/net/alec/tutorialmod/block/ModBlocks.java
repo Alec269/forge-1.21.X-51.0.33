0e7a8cae976ccae6faef95e63df61cf6a98b18aa
Alec <alac269b@gmail.com>
2025-02-09 00:38:49 +0600

    Added 2 ores,no gen

--- src/main/java/net/alec/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/alec/tutorialmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.alec.tutorialmod.block;
 
 import net.alec.tutorialmod.TutorialMod;
 import net.alec.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -25,15 +27,23 @@ public class ModBlocks {
 //2nd Block
   public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK = registerBlock(
   "raw_alexandrite_block",()-> new Block(BlockBehaviour.Properties.of()
-.strength(4f).sound(SoundType.COPPER)));
+.strength(3.5f).sound(SoundType.COPPER)));
 //3rd Block
+  public static final RegistryObject<Block> ALEXANDRITE_ORE = registerBlock(
+  "alexandrite_ore",()-> new DropExperienceBlock(UniformInt.of(1,5),BlockBehaviour.Properties.of()
+  .strength(3.5f).sound(SoundType.STONE))); // ^ xp drop Property  // ^ min and ^ max amount
+//4th Block
+public static final RegistryObject<Block> DEEPSLATE_ALEXANDRITE_ORE = registerBlock(
+"deepslate_alexandrite_ore",()-> new DropExperienceBlock(UniformInt.of(2,4),BlockBehaviour.Properties.of()
+.strength(5f).sound(SoundType.DEEPSLATE)));
+//5th
 
 
 ///Preparation For adding blocks
 /// Custom method for automating block creation
   private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
    RegistryObject<T> toReturn = BLOCKS.register(name, block);
-   registerBlockItem(name, toReturn);
+   registerBlockItem(name, toReturn); //Item register class called
    return toReturn;
   }
 /// Custom method for automating block item creation
